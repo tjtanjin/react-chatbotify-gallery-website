5a1d998a0d9c524f355c3207464f0da2241bcd66
NeonNature <mustachemaung@gmail.com>
2024-06-03 02:30:01 +0700

    refactor: separate css files

--- src/components/ThemeCard.tsx
+++ src/components/ThemeCard.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Theme } from '../interfaces/Theme';
 import { Link } from 'react-router-dom';
+import FavIcon from '../assets/images/icon_favorite.svg?react'
+import '../styles/theme_card.css'
 
 interface Props {
 	theme: Theme;
 }
 
 const ThemeCard: React.FC<Props> = ({ theme }) => {
+	const [isFav, setIsFav] = useState(false);
 	return (
-		<div className="bg-slate-50 text-black w-[300px] h-[545px] rounded">
-			<img src={theme.themeImg} className="w-full h-[400px]" alt={theme.name} />
-
-			<div className="p-2 flex flex-col justify-between h-[145px]">
+		<div className="text-black w-[300px] h-[545px]">
+			<img src={theme.themeImg} className="w-full h-[400px] scale-75 hover:-translate-y-6 transition ease-in-out duration" alt={theme.name} />
+			<div className="theme-card-info">
 				<div>
-					<p className="text-[15px] opacity-80 mb-2">{theme.name}</p>
+					<div className="flex justify-between">
+						<p className="theme-card-title">{theme.name}</p>
+						<button className={`theme-card-fav ${isFav && 'active'}`} onClick={() => setIsFav(fav => !fav)}>
+							<FavIcon />
+						</button>
+					</div>
 					<p className="text-[15px] opacity-80">{theme.description}</p>
 				</div>
 
 				<div className='flex justify-between'>
-					<h4 className="text-base font-medium mb-0">{theme.authorName}</h4>
+					<h4 className="theme-card-author">{theme.authorName}</h4>
 
 					<Link to={`/profile/${theme.github}`} className=' text-blue-500 visited:text-purple-600'>{theme.github}</Link>
 				</div>
