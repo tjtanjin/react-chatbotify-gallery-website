1142ada17a9fe9b01ecdb462340328dfa9207358
tjtanjin <cjtanjin@gmail.com>
2024-08-11 13:05:18 +0800

    docs: Minor update to code comments

--- src/services/apiService.tsx
+++ src/services/apiService.tsx
@@ -1,7 +1,13 @@
+/**
+ * Wraps around the window fetch function to always include credentials.
+ *
+ * @param url url to call
+ * @param options additional options passed to fetch
+ */
 const galleryApiFetch = (url: string, options = {}) => {
   return fetch(url, {
       ...options,
-      credentials: 'include', // Always include credentials
+      credentials: 'include',
   });
 }
 
