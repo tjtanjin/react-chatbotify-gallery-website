3aa41d619747f1665ab8952ad959972824cbcc95
adel <adeelguitoun@gmail.com>
2024-09-03 17:56:46 +0100

    feat: Create the about page component

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import HeroAbout from "../components/About/HeroAbout"
+
+const About: React.FC = () => {
+	return (
+		<div className="bg-slate-950 text-slate-50">
+			<div className="container px-6 mx-auto">
+				<HeroAbout />
+			</div>
+		</div>
+	)
+}
+export default About
