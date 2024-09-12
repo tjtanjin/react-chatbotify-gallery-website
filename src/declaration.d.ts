207370d1cd66ddb04b2aa5252e13b55e93ad851a
NamelessExplorer <91269749+NamelessExplorer@users.noreply.github.com>
2024-09-12 10:38:07 +0530

    Resolved some small issues

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 declare module "*.png"
 declare module '*.json' {
   const value: any;
   export default value;
 }
-=======
-declare module "*.png"
->>>>>>> 45d0bd74b629ed311466970d2ae9c435d1df47a8
