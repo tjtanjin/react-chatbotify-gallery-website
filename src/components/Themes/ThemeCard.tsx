20c986d851532c37d82eb33db4c19904d90d25e0
wlee261 <wlee@umail.ucsb.edu>
2024-09-14 04:47:30 -0700

    feat: mobile theme card

--- src/components/Themes/ThemeCard.tsx
+++ src/components/Themes/ThemeCard.tsx
@@ -7,82 +7,62 @@ import { Theme } from '../../interfaces/Theme';
 import FavIcon from '../../assets/images/icon_favorite.svg';
 import GitHubIcon from '../../assets/images/icon_github_white.svg';
 
-import '../../styles/theme_card.css'
+import '../../styles/theme_card.css';
 
 type Props = {
-  theme: Theme
-  isPreviewed: boolean
-  onPreview: (name: string) => void
-}
+  theme: Theme;
+  isPreviewed: boolean;
+  onPreview: (name: string) => void;
+};
 
 /**
  * Theme card component to hold the details of each theme in the themes page.
  */
 const ThemeCard: React.FC<Props> = ({ theme, isPreviewed, onPreview }) => {
-	const [isFav, setIsFav] = useState(false)
-	const [viewDetails, setViewDetails] = useState(false)
+	const [isFav, setIsFav] = useState(false);
+	const [viewDetails, setViewDetails] = useState(false);
 
 	const onViewDetails = () => {
-		setViewDetails(true)
-	}
+		setViewDetails(true);
+	};
 
 	const onClickPreview = () => {
-		onPreview(theme.name)
-	}
+		onPreview(theme.name);
+	};
+
+	const [isCardSelected, setIsCardSelected] = useState(false);
+
+	const handleCheckboxChange = () => {
+		setIsCardSelected(!isCardSelected);
+	};
 
 	return (
-		<>
-			<div className="text-black w-[300px] h-[545px]">
-				<button
-					onClick={onViewDetails}
-					type="button"
-					className="relative group"
-				>
-					<img
-						src={theme.themeImg}
-						className="cursor-pointer w-full h-[400px] scale-80
-              group-hover:-translate-y-6 transition ease-in-out duration"
-						alt={theme.name}
-					/>
-					<div className="theme-card-details">View Details</div>
-				</button>
-				<div className="theme-card-info">
-					<div>
-						<div className="flex justify-between">
-							<p className="theme-card-title">{theme.name}</p>
-							<button
-								type="button"
-								aria-label="Favorite Button"
-								className={`theme-card-fav ${isFav && 'active'}`}
-								onClick={() => setIsFav((fav) => !fav)}
-							>
-								<img src={FavIcon} alt="fav-icon" />
-							</button>
-						</div>
-						<p className="text-[15px] opacity-80">{theme.description}</p>
-					</div>
-					<Link to={`/profile/${theme.github}`} className="theme-card-github">
-						<img src={GitHubIcon} alt="github-icon" />
-					</Link>
-					<div className="flex justify-between">
-						<h4 className="theme-card-author">{theme.authorName}</h4>
-						<button
-							className={`theme-card-preview ${isPreviewed ? 'active' : ''}`}
-							type="button"
-							onClick={onClickPreview}
-						>
-							Preview
-						</button>
-					</div>
-				</div>
+		<div className={`flex flex-row p-4 my-4 mx-2 border-2 rounded-xl ${isCardSelected ? 'border-blue-700' : 'border-accent-600'}`}>
+			<div className="flex-1 basis-1/5 mr-3 flex flex-row overflow-hidden w-32 h-20 rounded-xl">
+				<img
+					src={theme.themeImg}
+					alt={theme.name}
+					className="w-60 h-60 object-cover object-left-top"
+				/>
 			</div>
-			<ThemeModal
-				isOpen={viewDetails}
-				theme={theme}
-				onClose={() => setViewDetails(false)}
-			/>
-		</>
-	)
-}
+			<div className="flex-1 mr-4 basis-3/5 flex flex-col justify-center">
+				<h2 className="text-accent-50">{theme.name}</h2>
+				<span className="text-accent-300">{theme.description}</span>
+				<span className="text-blue-500">More Info (i)</span>
+			</div>
+			<div className="flex-1 basis-1/5 flex items-center">
+				<label className="text-accent-50">
+					Select
+					<input
+						type="checkbox"
+						checked={isCardSelected}
+						onChange={handleCheckboxChange}
+						className="ml-2"
+					/>
+				</label>
+			</div>
+		</div>
+	);
+};
 
-export default ThemeCard
+export default ThemeCard;
--- src/pages/Themes.tsx
+++ src/pages/Themes.tsx
@@ -16,94 +16,106 @@ import { useSearchParams } from 'react-router-dom';
  * // todo: dynamically load themes as user scrolls instead of fetching wholesale from backend
  */
 const Themes: React.FC = () => {
-  //search param hook to access URL
-  const [searchParams, setSearchParams] = useSearchParams()
+	//search param hook to access URL
+	const [searchParams, setSearchParams] = useSearchParams();
 
 	// search query for filtering themes to show
-	const [searchQuery, setSearchQuery] = useState(()=> searchParams.get('searchQuery') || "");
+	const [searchQuery, setSearchQuery] = useState(
+		() => searchParams.get('searchQuery') || ''
+	);
 
 	// id of themes being selected to be preview (and applied to the interactive chatbot)
 	const [previewIds, setPreviewIds] = useState<string[]>([]);
 
 	// theme data fetched from backend
 
-	const { themes, loading, error } = useFetchData(Endpoints.fetchApiThemes, 30, 1, searchQuery);
+	const { themes, loading, error } = useFetchData(
+		Endpoints.fetchApiThemes,
+		30,
+		1,
+		searchQuery
+	);
 	/**
-	 * Handles setting of search query when user hits enter.
-	 *
-	 * @param query query user inputted
-	 */
+   * Handles setting of search query when user hits enter.
+   *
+   * @param query query user inputted
+   */
 	const handleSearch = (query: string) => {
-    if(query === ""){
-      searchParams.delete('searchQuery')
-    } 
-    else {
-      searchParams.set('searchQuery',query)
-    }
-    setSearchParams(searchParams)
+		if (query === '') {
+			searchParams.delete('searchQuery');
+		} else {
+			searchParams.set('searchQuery', query);
+		}
+		setSearchParams(searchParams);
 		setSearchQuery(query);
-	}
+	};
 
 	/**
-	 * Handles setting and unsetting of theme ids to preview.
-	 *
-	 * @param id id of theme to set or unset
-	 */
+   * Handles setting and unsetting of theme ids to preview.
+   *
+   * @param id id of theme to set or unset
+   */
 	const onPreview = (id: string) => {
 		setPreviewIds((prevPreviewId) =>
 			prevPreviewId.includes(id)
 				? prevPreviewId.filter((item) => item !== id)
 				: [...prevPreviewId, id]
-		)
-	}
+		);
+	};
 
 	/**
-	 * Clears all preview ids.
-	 */
+   * Clears all preview ids.
+   */
 	const clearPreviewIds = () => {
-		setPreviewIds([])
-	}
+		setPreviewIds([]);
+	};
 
 	// flow for interactive chatbot
 	const flow = {
 		start: {
 			message: (params: Params) => {
-				params.injectMessage("Hello 👋! Did you know? The order of specifying themes matters!")
-				return "Try previewing some themes below, or click on those on the left! 😊"
+				params.injectMessage(
+					'Hello 👋! Did you know? The order of specifying themes matters!'
+				);
+				return 'Try previewing some themes below, or click on those on the left! 😊';
 			},
-			checkboxes: {items: ["Minimal Midnight", "Cyborg", "Terminal"]},
+			checkboxes: { items: ['Minimal Midnight', 'Cyborg', 'Terminal'] },
 			function: (params: Params) => {
-				setPreviewIds(params.userInput.split(",").map(theme => {
-					if (theme === "Minimal Midnight") {
-						return "minimal_midnight"
-					} else if (theme === "Cyborg") {
-						return "cyborg"
-					} else {
-						return "terminal"
-					}
-				}))
+				setPreviewIds(
+					params.userInput.split(',').map((theme) => {
+						if (theme === 'Minimal Midnight') {
+							return 'minimal_midnight';
+						} else if (theme === 'Cyborg') {
+							return 'cyborg';
+						} else {
+							return 'terminal';
+						}
+					})
+				);
 			},
 			chatDisabled: true,
-			path: "end"
+			path: 'end',
 		},
 		end: {
 			message: "What's next? 😊",
-			options: ["Try Again", "Check Documentation", "Discord"],
+			options: ['Try Again', 'Check Documentation', 'Discord'],
 			path: (params: Params) => {
-				if (params.userInput === "Try Again") {
-					setPreviewIds([])
-				} else if (params.userInput === "Discord") {
-					window.open("https://discord.gg/6R4DK4G5Zh")
-				} else if (params.userInput === "Check Documentation") {
-					window.open("https://react-chatbotify.com")
+				if (params.userInput === 'Try Again') {
+					setPreviewIds([]);
+				} else if (params.userInput === 'Discord') {
+					window.open('https://discord.gg/6R4DK4G5Zh');
+				} else if (params.userInput === 'Check Documentation') {
+					window.open('https://react-chatbotify.com');
 				} else {
-					setPreviewIds([])
-					params.injectMessage("Hmmm I'm not sure what you said, but let's try again!")
+					setPreviewIds([]);
+					params.injectMessage(
+						"Hmmm I'm not sure what you said, but let's try again!"
+					);
 				}
-				return "start"
-			}
-		}
-	}
+				return 'start';
+			},
+		},
+	};
 
 	// todo: show a proper error message if themes are not able to be fetched
 	if (error) {
@@ -111,83 +123,33 @@ const Themes: React.FC = () => {
 	}
 
 	return (
-		<div className="flex flex-col lg:flex-row h-screen relative">
-			{/* Main content area */}
-			<div className="order-1 md:order-0 lg:w-3/4 overflow-y-auto bg-gray-900 p-8 hide-scrollbar">
-				<div
-					className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 pb-8
-						gap-6 justify-items-center w-full"
-					style={{transform: "translateY(8vh)"}}
-				>
-					
-					{loading
-						? Array.from({ length: 9 }).map((_, index) => (
-							<Skeleton key={`skeleton-${index}`} width="100%" containerClassName='w-full' height={400} />
-						))
-						: themes.map((theme: Theme) => (
+		<div className="bg-accent-950 flex h-screen w-full">
+			{/* Main Content Section */}
+			<div className="mt-20 overflow-y-scroll">
+				<div className="text-accent-50">
+					{/* Headers */}
+					<h1>Select Theme(s)</h1>
+					<h2>
+						You can select multiple themes and combine them however you like.
+					</h2>
+					<h2>How choosing multiple themes work (i)</h2>
+				</div>
+				<div>
+					{/* Card Content */}
+					{themes.map((theme) => {
+						return (
 							<ThemeCard
 								key={theme.id}
 								theme={theme}
 								isPreviewed={previewIds.includes(theme.id)}
 								onPreview={() => onPreview(theme.id)}
 							/>
-						))}
+						);
+					})}
 				</div>
-			</div>
-
-			{/* Pinned search column */}
-			<div className="flex flex-col order-0 lg:order-1">
-				<div className="bg-black w-full" style={{height: "9vh"}}></div>
-				<div className="bg-white shadow-xl p-6 flex flex-col overflow-y-auto hide-scrollbar">
-					<div className="mb-4">
-						<SearchBar onSearch={handleSearch} />
-					</div>
-					<div className="lg:block hidden">
-						<div className="flex flex-col justify-center items-center bg-gray-100 rounded-lg p-4">
-							<ChatBot
-								flow={flow}
-								themes={previewIds.map(themeId => ({ id: themeId }))}
-								settings={{ general: { embedded: true } }}
-								styles={{ chatWindowStyle: { height: '60vh', width: '20vw' } }}
-							/>
-							<button 
-								onClick={clearPreviewIds} 
-								className="text-white p-2 rounded-lg mt-5"
-								style={{backgroundColor: "#491d8d"}}
-							>
-								Clear Previews
-							</button>
-						</div>
-						<div className="flex flex-col justify-between items-center mb-4 mt-5 w-full">
-							<div className="bg-gray-100 rounded-lg p-4 overflow-y-auto shadow-lg w-full max-w-md">
-								<h2 className="text-center font-bold mb-4 text-lg text-gray-800">Preview Themes</h2>
-								{previewIds.length > 0 ? (
-									<ul className="divide-y divide-gray-300">
-										{previewIds.map((id, index) => (
-											<li
-												key={index}
-												className="p-3 flex items-center justify-between text-gray-700
-													hover:bg-gray-200 rounded transition duration-150"
-											>
-												<span>{id}</span>
-											</li>
-										))}
-									</ul>
-								) : (
-									<p className="text-center text-gray-500">No themes selected for preview</p>
-								)}
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			{/* Modal container */}
-			<div id="modal-container" className="fixed inset-0 z-50 pointer-events-none">
-				{/* ThemeModals will be rendered here by React's portal */}
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default Themes
+export default Themes;
